8d626764c1d9852603ff9b489575337ff63440e6
Deepak Mahesh Koilada <126042551+deepakmaheshkoilada@users.noreply.github.com>
2026-04-10 20:20:00 +0530

    Update ceasarCipher.py

--- CODECRAFT_CS_01/ceasarCipher.py
+++ CODECRAFT_CS_01/ceasarCipher.py
@@ -1,1 +1,59 @@
+small_alphabets = 'abcdefghijklmnopqrstuvwxyz'
+cap_alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+def encrypt(text, key):
+    cipherText = ''
+    for letter in text:
+        if not letter == ' ':
+            if ord(letter)>=65 and ord(letter)<=90:
+                index = cap_alphabets.find(letter)
+                new_index = index + key
+                if new_index>=26:
+                    new_index -= 26
+                cipherText += cap_alphabets[new_index]
+            elif ord(letter)>=97 and ord(letter)<=122:
+                index = small_alphabets.find(letter)
+                new_index = index + key
+                if new_index>=26:
+                    new_index -= 26
+                cipherText += small_alphabets[new_index]
+            else:
+                cipherText += letter
+    return cipherText
+
+def decrypt(text, key):
+    plainText = ''
+    for letter in text:
+        if not letter == ' ':
+            if ord(letter)>=65 and ord(letter)<=90:
+                index = cap_alphabets.find(letter)
+                new_index = index - key
+                if new_index>=26:
+                    new_index += 26
+                plainText += cap_alphabets[new_index]
+            elif ord(letter)>=97 and ord(letter)<=122:
+                index = small_alphabets.find(letter)
+                new_index = index - key
+                if new_index>=26:
+                    new_index += 26
+                plainText += small_alphabets[new_index]
+            else:
+                plainText += letter
+    return plainText
+
+print('*****Ceasar Cipher*****')
+
+mode = input('Enter Mode (e/d): ')
+
+if mode=='e':
+    print("Encryption Mode Enabled.")
+    text = input('Enter text to be encrypted: ')
+    key = int(input('Enter key: '))
+    result = encrypt(text, key)
+    print(f'Cipher Text: {result}')
+elif mode=='d':
+    print("Decryption Mode Enabled.")
+    text = input('Enter text to be decrypted: ')
+    key = int(input('Enter key: '))
+    result = decrypt(text, key)
+    print(f'Plain Text: {result}')
